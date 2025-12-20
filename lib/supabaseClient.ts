fd9e4cff3a2882774657812f8fd3dcc88194acbd
Trevor muchel <trevormichel8@gmail.com>
2025-12-20 10:06:48 -0700

    Create supabaseClient.ts

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabaseClient = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+
+export function supabaseServer() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
