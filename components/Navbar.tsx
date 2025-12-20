8c327445ab0c5ff3a0744143c9a7ccde9ead75e2
Trevor muchel <trevormichel8@gmail.com>
2025-12-20 10:35:53 -0700

    Create Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <a href="/">Generator</a>
+      <a href="/gallery">Gallery</a>
+      <a href="/explore">Explore</a>
+      <a href="/login">Login</a>
+    </nav>
+  );
+}
